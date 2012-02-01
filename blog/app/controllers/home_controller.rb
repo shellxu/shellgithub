742681f080f6d5802b8113ae6c6c73f8975d3eb1
sxu <sxu@visionnexus.com>
2012-01-31 16:55:19 -1000

    rails blog

---
+++ blog/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+	print "AAAAA"
+  end
+end
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '983d7550d4fc27b610efccdb9f5316a6a1fccaf0f18151216eb8049e818320ab9d7bbbd72a322d0bee36108c9663031b6123edf8541e1fa2ec09089719c7f6db'
